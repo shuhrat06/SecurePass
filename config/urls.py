7443f507931df41babac1077f28b79ebb97dee1e
shuhrat06 <shuhratrozimatov06@gmail.com>
2026-05-10 23:04:29 +0500

    ............

--- config/urls.py
+++ config/urls.py
@@ -4,11 +4,18 @@ from main.views import *
 from django.conf import settings
 from django.conf.urls.static import static
 from main.views import *
+from django.urls import re_path
 
 urlpatterns = [
     path('kjeshfkajh4aajfalajsdfjkj1546asf41dfs/', admin.site.urls),
     path('', include('main.urls')),
     path('auth/', include('users.urls')),
 ]
-
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT )
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT )
+else:
+    urlpatterns += [
+        re_path(r'^media/(?P<path>.*)$', serve, {
+            'document_root': settings.MEDIA_ROOT
+        }),
+    ]
