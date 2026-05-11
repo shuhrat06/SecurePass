d8899054c67b3d752d27fb2d382e86f5be98093b
shuhrat06 <shuhratrozimatov06@gmail.com>
2026-05-11 22:17:30 +0500

    ...

--- config/settings.py
+++ config/settings.py
@@ -12,7 +12,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-xkm6(bo^rm7rbwxo3%o16i2#+4*39to@9&6&c@uk-f4gd$c5r8'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
@@ -59,7 +59,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
-                'main.context_processors.get_msgs_count'
+                'main.context_processor.get_msgs_count'
             ],
         },
     },
--- main/context_processor.py
+++ main/context_processor.py
@@ -2,8 +2,11 @@ from .models import Messages
 from django.db.models import Q
 
 def get_msgs_count(request):
-    msgs = Messages.objects.filter(Q(for_all=True) | Q(user=request.user))
-    unread_count = msgs.filter(viewed=False).count()
+    if request.user.is_authenticated:
+        msgs = Messages.objects.filter(Q(for_all=True) | Q(user=request.user))
+        unread_count = msgs.filter(viewed=False).count()
+    else:
+        unread_count = 0
     context = {
         'unread_count': unread_count
     }
